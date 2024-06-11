3139a840e1d8612ac41588179fc12a9336fb4b04
karolhof <2303211@ucc.edu.ar>
2024-06-10 21:19:53 -0300

    arreglos en el alta, baja y op de las cuentas

--- main.cpp
+++ main.cpp
@@ -2,20 +2,44 @@
 #include "clases.h"
 using namespace std;
 
-void altaCuenta(Cliente& cliente) {
-    float saldoPeso, saldoDolar;
-    cout << "Ingrese el saldo inicial en pesos: ";
-    cin >> saldoPeso;
-    cout << "Ingrese el saldo inicial en dolares: ";
-    cin >> saldoDolar;
-    Cuenta nuevaCuenta(saldoPeso, saldoDolar);
-    cliente.cuent = nuevaCuenta;
-    cout << "Cuenta creada exitosamente." << endl;
+void altaCuenta(Cliente array[], int nroCliente, Cliente& cliente) {
+
+    bool encontrado = true;
+    for (int j = 0; j < 100; j++) {
+        if (array[j].getDni() == nroCliente) {
+            encontrado = true;
+            cout << "Ese cliente ya posee una cuenta de banco" << endl;
+            break;
+        }
+    }
+    if (!encontrado) {
+        float saldoPeso, saldoDolar;
+        cout << "Ingrese el saldo inicial en pesos: ";
+        cin >> saldoPeso;
+        cout << "Ingrese el saldo inicial en dolares: ";
+        cin >> saldoDolar;
+        Cuenta nuevaCuenta(saldoPeso, saldoDolar);
+        cliente.cuent = nuevaCuenta;
+        cout << "Cuenta creada exitosamente." << endl;
+    }
+    
 }
 
-void bajaCuenta(Cliente& cliente) {
-    cliente.estado=0; // Reinicia la cuenta a 0
-    cout << "Cuenta dada de baja exitosamente." << endl;
+
+//VER!!!!!!!!!!!!!!!
+void bajaCuenta(Cliente array[], int nroCliente) {
+    bool encontrado = false;
+    for (int j = 0; j < 100; j++) {
+        if (array[j].getDni() == nroCliente) {
+            encontrado = true;
+            array[j].estado= 0;
+            cout << "Cliente dado de baja exitosamente." << endl;
+            break;
+        }
+    }
+    if (!encontrado) {
+        cout << "No hay un cliente del banco con ese número." << endl;
+    }
 }
 
 void solicitarFecha(int& _mes, int& _ano) {
@@ -42,17 +66,11 @@ void solicitarFecha(int& _mes, int& _ano) {
 }
 
 void menuCliente(Cliente array[], int &i) {
+    //VER (tratar de que solicite los datos del cliente al hacer las altas y bajas)
     int mes, ano, nro;
-    Cuenta Cu(1000, 500);
-
-    cout << "Ingrese el nro de Cliente (su DNI): "<<endl;
-    cin >> nro;
-
-    Cliente Cl(nro, "Juan Perez", 2020, 1, 2, Cu);
-    array[i] = Cl;
-    i++;
     int caja, op, monto;
-
+    Cuenta Cu(1000, 500);
+    
     cout << "Desea: " << endl;
     cout << "1=Dar de alta una cuenta" << endl;
     cout << "2=Dar de baja una cuenta" << endl;
@@ -60,27 +78,44 @@ void menuCliente(Cliente array[], int &i) {
     cout << "4=Salir" << endl;
     cin >> op;
 
+
+    cout << "Ingrese el nro de Cliente (su DNI): "<<endl;
+    cin >> nro;
+
+    Cliente Cl(nro, "Juan Perez", 2020, 1, 2, Cu);
+    array[i] = Cl;
+    i++;
+    
+
+    
     switch (op) {
         case 1:{
-            altaCuenta(array[i-1]);
+            altaCuenta(array,nro,array[i-1]);
             break;
         }
         case 2:{
-            bajaCuenta(array[i-1]);
-            // Establecer método de búsqueda de cliente
+            bajaCuenta(array, nro);
             break;
         }
         case 3: {
-            cout << "¿En que caja desea operar? (1=peso, 2=dolar)" << endl;
-            cin >> caja;
-            cout << "¿Que operacion desea realizar?(1=deposito, 2=extraccion)" << endl;
-            cin >> op;
-            cout << "Ingrese el monto" << endl;
-            cin >> monto;
-            solicitarFecha(mes, ano);
-            Cl.realizTrans(monto, op, caja, mes, ano);
-            Transaccion Tr(mes, ano, op, monto);
-            Cl.cuent.t.getMonto();
+            for (int j=0; j < (i-1); j++){
+                if ((array[j].getDni() == nro) && (array[j].estado!= 0)){
+                    cout << "¿En que caja desea operar? (1=peso, 2=dolar)" << endl;
+                    cin >> caja;
+                    cout << "¿Que operacion desea realizar?(1=deposito, 2=extraccion)" << endl;
+                    cin >> op;
+                    cout << "Ingrese el monto" << endl;
+                    cin >> monto;
+                    solicitarFecha(mes, ano);
+                    Cl.realizTrans(monto, op, caja, mes, ano);
+                    Transaccion Tr(mes, ano, op, monto);
+                    Cl.cuent.t.getMonto();
+                }
+                if ((array[j].getDni() == nro) && (array[j].estado== 0)){
+                cout<<"EL cliente está dado de baja."<<endl;
+            }
+            }
+    
             break;
         }
         default:{
